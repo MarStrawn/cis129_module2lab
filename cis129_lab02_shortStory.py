b8a920e2a8401845c4ca55ffe7f9e71944f2c887
MarStrawn <mstrawn@mail.pima.edu>
2024-09-26 20:15:11 -0700

    Update cis129_lab02_shortStory.py

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,6 @@
-print(Hello, World!)
+print(There was a Deaf man who wandered around the world searching for acceptance of being deaf because he was not born deaf, he went through hearing loss after getting a severe disease.)
+print(He did not know how to deal with his hearing loss because he did not grow up in a Deaf environment, did not know American Sign Language or any sign language, and felt that he lost his place in the world.)
+print(During his adventure, he met this deaf and blind man who turned out to be a monk but when he approached him, this monk understood his situation completely without even speaking to him.)
+print(The monk had him do a lot of yoga to connect with nature and his spiritual self inside of him, but he did not want to at first because he was in a hurry to find his answer.)
+print(The monk revealed to him the reason why he can still communicate with him and it was because he also went through multiple diseases that caused him lost his hearing and sight, but explained that being disabled does not have to stop you from living and chasing goals.)
+print(It took some time, but he realized that being deaf did not mean that he had to give up on living, he can still do everything, but hear so he started looking forward to his future and got excited for what is ahead.)
